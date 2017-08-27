401784b881ee6517f0ac2f5f426a3a8bf002460f
Bruno Silva <brunogs.sp@gmail.com>
2017-08-26 22:23:53 -0300

    add: project generated by start.spring.io

---
+++ src/main/java/br/com/belezanaweb/jpa/jpalecture/JpaLectureApplication.java
@@ -0,0 +1,12 @@
+package br.com.belezanaweb.jpa.jpalecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLectureApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/belezanaweb/jpa/jpalecture/JpaLectureApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.belezanaweb.jpa.jpalecture;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaLectureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
